66e38994649f89084858899682507e9c0f1f14b4
Shweta Mishra <shmis2004@gmail.com>
2022-09-28 18:11:58 +0530

    fruitjuice

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+const fruitJuice = function (apple, mango) {
+  console.log(
+    `number of apples and oranges used in juice is respectively ${apple} and ${mango}`
+  );
+};
+
+fruitJuice(2, 4);
